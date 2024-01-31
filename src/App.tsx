a30d0e01c9e9ec613464523659004e845d78495c
waleedo0o7 <waleedhd1@gmail.com>
2024-01-31 17:26:14 +0200

    finish table for test

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,27 @@ import Login from "./layouts/public-layout/pages/login";
 import NotFound from "./layouts/public-layout/pages/notFoundPage";
 import SuperAdminLayout from "./layouts/superAdmin/pages/superAdmin.layout";
 import Admin from "./layouts/superAdmin/pages/admin";
+import DesignElements from "./shared/designElements";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<PublicLayout />}>
           <Route index element={<Home />} />
           <Route path="contact" element={<Contact />} />
+          <Route path="design-elements" element={<DesignElements />} />
           <Route path="login" element={<Login />} />
           <Route path="*" element={<NotFound />} />
         </Route>
+
         <Route element={AuthGuard()}>
           <Route path="/admin" element={<SuperAdminLayout />}>
             <Route index element={<Admin />} />
           </Route>
         </Route>
+
       </Routes>
     </BrowserRouter>
   );
